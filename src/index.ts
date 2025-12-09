5b60ea83dca1016fb4eb1d82d1fc831886e69003
Marco <marcos.lodato@alumnos.uai.edu.ar>
2025-12-08 21:44:21 -0300

    Render Jp

--- src/index.ts
+++ src/index.ts
@@ -18,6 +18,7 @@ app.use(express.json({ limit: '10mb' }));
 app.use(cors({
     origin: [
         'https://dailygrinddfront.vercel.app',
+        "https://daily-grind-front-329e.vercel.app",
         'http://localhost:5173'
     ],
     credentials: true,
