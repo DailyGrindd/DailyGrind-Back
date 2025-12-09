faa475c7d022c538b6d4dabd62a391669c3e1ef1
Marco <marcos.lodato@alumnos.uai.edu.ar>
2025-12-08 21:07:30 -0300

    Arreglo Cors

--- src/index.ts
+++ src/index.ts
@@ -21,7 +21,7 @@ app.use(cors({
         'http://localhost:5173'
     ],
     credentials: true,
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization', 'Cookie'],
     exposedHeaders: ['Set-Cookie']
 }));
